6f9572228f75a1f3fe863a3e432694fef6e6f362
Gregory Kolb <gkolb100@gmail.com>
2025-07-05 16:27:00 -0600

    add svelte frontend

---
+++ apps/frontend/vite.config.ts
@@ -0,0 +1,21 @@
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+// Recreate __filename and __dirname
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+import { defineConfig } from 'vite';
+import { svelte } from '@sveltejs/vite-plugin-svelte';
+import { nxViteTsPaths } from '@nx/vite/plugins/nx-tsconfig-paths.plugin';
+
+export default defineConfig({
+  root: __dirname,
+  plugins: [svelte(), nxViteTsPaths()],
+  build: {
+    outDir: '../../dist/apps/frontend',
+  },
+  server: {
+    port: 4200,
+  },
+});
