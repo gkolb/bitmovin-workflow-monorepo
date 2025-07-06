45b7f4e9aa2ecdb5524aedcb78a41c1e71c19917
Gregory Kolb <gkolb100@gmail.com>
2025-07-05 21:52:01 -0600

    add simple bitmovin player with default ui

--- apps/player/src/App.svelte
+++ apps/player/src/App.svelte
@@ -1,4 +1,4 @@
-<script lang="ts">
+<!-- <script lang="ts">
   let count: number = 0;
   const increment = () => {
     count += 1;
@@ -7,4 +7,13 @@
 
 <button on:click={increment}>
   count is {count}
-</button>
+</button> -->
+<script lang="ts">
+  import Player from './Player.svelte';
+
+  const source = {
+    dash: 'https://cdn.bitmovin.com/content/assets/art-of-motion-dash-hls-progressive/mpds/f08e80da-bf1d-4e3d-8899-f0f6155f6efa.mpd'
+  };
+</script>
+
+<Player {source} />
